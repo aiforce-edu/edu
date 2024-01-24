bb36552054fc6579c9f907960a1e07e3ffff2380
aiforce-edu <137736191+aiforce-edu@users.noreply.github.com>
2024-01-24 15:10:01 +0900

    Create RGB_lamp_kit.ino

---
+++ _code/RGB_lamp_kit.ino
@@ -0,0 +1,23 @@
+#include <Adafruit_NeoPixel.h>
+
+int RED 	 = 0;
+int GREEN  = 0;
+int BLUE   = 0;
+int RGBled = 2; 
+int num = 12;
+Adafruit_NeoPixel pixels(num, RGBled, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+  int valRED   = analogRead(RED);
+  int valGREEN = analogRead(GREEN);
+  int valBLUE  = analogRead(BLUE);
+
+  for (int i=0 ; i<num ; i++)
+     pixels.setPixelColor(i, pixels.Color(valRED/4, valGREEN/4, valBLUE/4));
+
+  pixels.show();
+}
